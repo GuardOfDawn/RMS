75af73df802050ec8f616fd73453151c81a6f23d
fx786595833 <786595833@qq.com>
2016-11-20 12:21:49 +0800

    实现

---
+++ CourseHomeworkCheckSystem/src/newproject/service/dao/RADao.java
@@ -0,0 +1,17 @@
+package newproject.service.dao;
+
+import java.util.List;
+
+import newproject.model.RA;
+
+public interface RADao {
+	public boolean insert(RA ra);
+	
+	public boolean remove(RA ra);
+	
+	public boolean modify(RA ra);
+	
+	public List<RA> findById(String userId);
+	
+	public List<RA> findByProject(String pid);
+}
